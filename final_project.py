d66bf4a6fb4efcf6df59769a6970916157062c78
Michelle Zheng <michellezheng@Michelles-MacBook-Pro-3.local>
2025-04-08 17:34:19 -0400

    adding names - Michelle

--- final_project.py
+++ final_project.py
@@ -2,8 +2,8 @@
 # 
 # Title: Gas-prices, Walk Scores, and Transit Scores in American Cities
 # Team: S.U.I.T (Students of Urban Information Technology)
-# 
-# 
+# Team Members: Faris Khojah, Sierra Seidel, Michelle Zheng
+#
 # 
 # 
 # 
