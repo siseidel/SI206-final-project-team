972035981283a033bfe122082b70018eba9cda39
Michelle Zheng <michellezheng@Michelles-MacBook-Pro-3.local>
2025-04-10 18:05:35 -0400

    Added abbreviations dictionary and def US_state_city

--- final_project.py
+++ final_project.py
@@ -7,20 +7,85 @@
 # 
 # 
 import json
+import requests 
 
-with open('US_States_and_Cities.json', 'r') as file:
-    data = json.load(file)
+# house keeping
 
-state_city_pairs = []
+state_abbreviations = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "District of Columbia": "DC",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY"
+}
 
-for state, cities in data.items():
-    selected_cities = cities[:3]
-    for city in selected_cities:
-        state_city_pairs.append((state, city))
+def US_state_city(jsonfile):
+    jsonfile = 'US_States_and_Cities.json'
+    with open(jsonfile, 'r') as file:
+        data = json.load(file)
+
+    state_city_pairs = []
+
+    for state, cities in data.items():
+        selected_cities = cities[:3]
+        for city in selected_cities:
+            state_city_pairs.append((state, city))
+            if len(state_city_pairs) == 100:
+                break
         if len(state_city_pairs) == 100:
             break
-    if len(state_city_pairs) == 100:
-        break
+    cityList = []
+    for state, city in state_city_pairs:
+        cityList.append((state, city))
+    # print(f"{cityList}")
+    return cityList
 
-for state, city in state_city_pairs:
-    print(f"State: {state}, City: {city}")
+
+
+
